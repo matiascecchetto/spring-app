f79aefb60924c42745334c9e5e26f9e534c5b9df
Matias Cecchetto <matias.cec@gmail.com>
2018-03-05 20:14:50 +1000

    Created simple REST API

---
+++ src/main/java/app/PersonRepository.java
@@ -0,0 +1,16 @@
+package app;
+
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+
+	List<Person> findByLastName(@Param("surname") String surname);
+  List<Person> findByFirstName(@Param("name") String name);
+  List<Person> findById(@Param("id") long id);
+
+}
